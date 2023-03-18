78af08e27da67b4c78641797908092ed5afe6a52
Jose Valle <jose_vr_th98@hotmail.com>
2023-03-18 11:27:14 -0600

    EJERCICIO 1

--- Laboratorio04/Program.cs
+++ Laboratorio04/Program.cs
@@ -4,9 +4,9 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Nombre: ");
-            Console.WriteLine("Carnet: ");
-            Console.WriteLine("Seccion: ");
+            Console.WriteLine("Nombre: JOSE RODRIGO VALLE RIVEIRO");
+            Console.WriteLine("Carnet: 1149123");
+            Console.WriteLine("Seccion: 04");
         }
     }
 }
